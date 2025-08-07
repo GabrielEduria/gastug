54ea1e44fd16f121629c2afb079ff8c3a41b229e
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-08-07 13:21:10 +0800

    added

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar(){
        <section className="w-full h-[45px] mb-30">
           <div className="max-w-[880px] mx-auto flex flex-end justify-between  gap-5 sm:gap-20 px-2 overflow-hidden py-3">
               <Link href="/">
-                <p className="flex cursor-pointer hover:opacity-50 dark:text-gray-500 hover:underline decoration-[2px] underline-offset-[3px] md:underline-offset-[5px] lg:text-xl pr-5">
+                <p className="flex text-white cursor-pointer hover:opacity-50 hover:underline decoration-[2px] underline-offset-[3px] md:underline-offset-[5px] lg:text-xl pr-5">
                   <BsArrowLeft className="text-2xl pt-1 lg:text-3xl lg:pt-1 pr-2 hover:underline"/>
                   back
                 </p>
@@ -24,7 +24,7 @@ export default function NavBar(){
                    {cleanedPath} 
                   </span> side-project By 
                   <a href="http://gabrieleduria.com"
-                    className="hover:scale-90 hover:underline "
+                    className="hover:scale-90 hover:underline hover:opacity-50"
                   > Gabbyyyy </a>
                 </h2>
               </div>
--- src/projects/RockPaperScissors.tsx
+++ src/projects/RockPaperScissors.tsx
@@ -20,24 +20,28 @@ export default function RockPaperScissors() {
                     <p> Your Score: <span id="player-score" className="text-yellow-500">0</span></p>
                     <p> Opponent Score: <span id="computer-score" className="text-yellow-500">0</span></p>   
                 </div> 
-                <div className="">
-
-                </div>
+           
         
-                <div className="flex justify-around px-10">
-                    <div id="rock" className="cursor-pointer hover:scale-105 transition duration-0.3s">
-                    <FaHandRock className="text-yellow-500 text-7xl"/>
+                <div className="flex justify-around p-10">
+                    <div id="rock" className="cursor-pointer hover:scale-105 transition duration-0.1s hover:opacity-80">
+                    <FaHandRock className="text-yellow-500 text-3xl lg:text-7xl"/>
                     <p className="text-center text-white text-3xl font-bold">ROCK</p>
                     </div>
-                    <div id="paper" className="cursor-pointer hover:scale-105 transition duration-0.3s">
+                    <div id="paper" className="cursor-pointer hover:scale-105 transition duration-0.1s hover:opacity-80">
                     <FaHandPaper className="text-yellow-500 text-7xl"/>
                     <p className="text-center text-white text-3xl font-bold">Paper</p>
                     </div>
-                    <div id="scissors" className="cursor-pointer hover:scale-105 transition duration-0.3s">
+                    <div id="scissors" className="cursor-pointer hover:scale-105 transition duration-0.1s hover:opacity-80">
                     <FaHandScissors className="text-yellow-500 text-7xl"/>
                     <p className="text-center text-white text-3xl font-bold">Rock</p>
                     </div>
                 </div>
+
+               {
+                <div className="border border-white">
+                    <p id="">You won!</p>
+                </div>
+                }
             </div>
     )
 }
