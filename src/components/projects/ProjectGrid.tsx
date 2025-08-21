776029b1ea34bd612d18b620b440636526c004c3
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-08-21 23:59:08 +0800

    inayos

--- src/components/projects/ProjectGrid.tsx
+++ src/components/projects/ProjectGrid.tsx
@@ -2,19 +2,19 @@ import ProjectCard from "./ProjectCard";
 import { projects } from "@/lib/projects";
 
 export default function ProjectGrid() {
+  
     return (
-<div className="flex justify-center">
-  <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center ">
-    {projects.map((project) => (
-        <ProjectCard
-        key={project.slug}
-        title={project.title}
-        description={project.description}
-        slug={project.slug}
-        />
-    ))}
-  </div>
+      <div className="flex justify-center">
+        <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center ">
+          {projects.map((project) => (
+              <ProjectCard
+              key={project.slug}
+              title={project.title}
+              description={project.description}
+              slug={project.slug}
+              />
+          ))}
+        </div>
 </div>
-
     )
 }
