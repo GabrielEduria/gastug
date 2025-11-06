65bd7cd7df5ecc45737c48d01b90d4d4d42566ed
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-06 22:50:52 +0800

    added border for a new component

--- src/projects/game2048.tsx
+++ src/projects/game2048.tsx
@@ -1,7 +1,7 @@
 export default function game2048() {
     return(
-     <div className="relative">
+        <div className="relative border border-white h-full">
 
-    </div>
+        </div>
     );
 };
